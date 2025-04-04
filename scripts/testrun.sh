5e62abb933cd81c6d7f8728c8e4f6e0979982a8d
route250 <hanaya.white@gmail.com>
2025-04-04 10:59:13 +0900

    update testrun.sh

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -4,6 +4,8 @@ PRJDIR=$(cd $(dirname $0)/..;pwd)
 LOGDIR=$PRJDIR/tmp/testlogs
 OS_DIST=$($PRJDIR/scripts/detectos.sh)
 CMD=$PRJDIR/dist/$OS_DIST/xclock_overlay
+BROWSER=google-chrome
+BR_OPT=""
 if [ ! -x "$CMD" ]; then
     echo "ERROR: can not found xclock_overlay:$CMD"
     exit 1
@@ -12,8 +14,8 @@ if ! type Xvnc >/dev/null 2>&1; then
     echo "ERROR: can not found Xvnc"
     exit 1
 fi
-if ! type google-chrome >/dev/null 2>&1; then
-    echo "ERROR: can not found google-chrome"
+if ! type $BROWSER >/dev/null 2>&1; then
+    echo "ERROR: can not found $BROWSER"
     exit 1
 fi
 
@@ -24,8 +26,8 @@ function fn_cleanup() {
   if [ -n "$pid_vnc" ]; then
     kill -9 "$pid_vnc"
   fi
-  if [ -n "$pid_chrome" ]; then
-    kill -9 "$pid_chrome"
+  if [ -n "$pid_browser" ]; then
+    kill -9 "$pid_browser"
   fi
 }
 trap fn_cleanup EXIT
@@ -36,9 +38,14 @@ sleep 1
 origDISPLAY="$DISPLAY"
 
 export DISPLAY=:7
-$CMD &
-google-chrome >$LOGDIR/chrome.log 2>&1 &
-pid_chrome=$!
+#if type mutter >/dev/null 2>&1; then
+#    mutter >$LOGDIR/mutter.log 2>&1 &
+#fi
+$CMD >$LOGDIR/xclock_overlay.log 2>&1 &
+export MOZ_DISABLE_GPU_ACCEL=1
+export MOZ_WEBRENDER=0
+$BROWSER $BR_OPT >$LOGDIR/browser.log 2>&1 &
+pid_browser=$!
 
 export DISPLAY="$origDISPLAY"
 vncviewer :7 >$LOGDIR/viewer.log 2>&1
